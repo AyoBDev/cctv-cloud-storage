d38c4012129b828c1f27fd22a3249493933a5bce
AyoBDev <ayobamitunmise99@gmail.com>
2026-05-09 15:20:38 +0100

    fix: wire up Rekognition/S3/SES clients to service modules on startup

--- src/plugins/aws.ts
+++ src/plugins/aws.ts
@@ -7,6 +7,9 @@ import { RekognitionClient } from '@aws-sdk/client-rekognition';
 import { S3Client } from '@aws-sdk/client-s3';
 import { SESClient } from '@aws-sdk/client-ses';
 import { env } from '@config/env';
+import { setRekognitionClient } from '@services/rekognition.service';
+import { setS3Client } from '@services/s3.service';
+import { setSesClient } from '@services/ses.service';
 
 export default fp(async function awsPlugin(app: FastifyInstance) {
   const kvs = new KinesisVideoClient({ region: env.AWS_REGION });
@@ -15,6 +18,11 @@ export default fp(async function awsPlugin(app: FastifyInstance) {
   const rekognition = new RekognitionClient({ region: env.AWS_REGION });
   const s3 = new S3Client({ region: env.AWS_REGION });
   const ses = new SESClient({ region: env.SES_REGION });
+
+  // Set clients on service modules
+  setRekognitionClient(rekognition);
+  setS3Client(s3);
+  setSesClient(ses);
 
   app.decorate('kvs', kvs);
   app.decorate('kms', kms);
